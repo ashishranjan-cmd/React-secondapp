52c92218941cc05c307145942322958d6b049738
Ashish Ranjan <akp9801@gmail.com>
2022-12-16 23:11:01 +0530

    date command

--- src/index.js
+++ src/index.js
@@ -4,4 +4,14 @@ var ReactDOM = require("react-dom")
 let fnm = "Tom";
 let lnm = "Jerry";
 
-ReactDOM.render(<h1>Welcome {fnm} & {lnm}</h1>, document.getElementById("root"))
+let mydate = new Date();
+let year = mydate.getFullYear();
+
+let mys= { color : 'Red' } // attribute
+
+ReactDOM.render(
+<div>
+  <h1>Welcome {fnm} & {lnm}</h1>
+  <p style={mys}>Current year is : {year}</p>
+</div>
+, document.getElementById("root"))
