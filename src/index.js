bdcba6c5b0a8ab8f935715db6f8a0a8796f34086
Ashish Ranjan <akp9801@gmail.com>
2022-12-17 12:05:58 +0530

    import and export variable from other file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 var React = require("react")
 var ReactDOM = require("react-dom")
 
+// import './index.css'; // path to css file
+
+import myname from './my.js'; // importing variable from other file
+
 let fnm = "Tom";
 let lnm = "Jerry";
 
@@ -9,9 +13,15 @@ let year = mydate.getFullYear();
 
 let mys= { color : 'Red' } // attribute
 
+let mypic = "https://picsum.photos/200";
+
+
 ReactDOM.render(
 <div>
   <h1>Welcome {fnm} & {lnm}</h1>
+  <p className='myc'> Testing the web</p>
   <p style={mys}>Current year is : {year}</p>
+  <img src={ mypic + "?grayscale" } />
+  <p>this is {myname} </p>
 </div>
 , document.getElementById("root"))
