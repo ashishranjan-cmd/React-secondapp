46ebe6a83ae243f3cc2ff6df9c4d690f9ada142d
Ashish Ranjan <akp9801@gmail.com>
2022-12-17 12:41:14 +0530

    all function in a single variable

--- src/index.js
+++ src/index.js
@@ -5,7 +5,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import myname, {fn, fn2} from './my.js'; // importing variable from other file
 import './index.css'; // path to css file
+import def1 from './my.js';
 
+import * as myf from './my.js'; // importing all function in a single variable
 
 let fnm = "Tom";
 let lnm = "Jerry";
@@ -25,5 +27,7 @@ ReactDOM.render(
   <p style={mys}>Current year is : {year}</p>
   <img src={ mypic + "?grayscale" } />
   <p>this is {myname} {fn()}</p>
+  <p>this is default variable :- {def1}</p>
+  <p>This is {myf.default} & {myf.fn()}</p>
 </div>
 , document.getElementById("root"))
